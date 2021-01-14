6da0b709bd7a47e8969255bb4c1c673b5c2976f5
hwasurr <iamsupermazinga@gmail.com>
2021-01-14 19:32:51 +0900

    아프리카 분석 추가

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 require('dotenv').config(); 
 console.log("크리에이터 상세정보 수집기 init!");
-require('./javascripts/getCreatorDetailData')();
+require('./javascripts/getCreatorDetailData')()
+  .then(() => {
+    require('./javascripts/getCreatorDetailData.afreeca')().then(() => {
+      process.exit(0);
+    })
+  })
